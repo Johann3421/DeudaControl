e695a8aacf4bf65c5e995244ce10685ee1f26013
Johann3421 <1355430@senati.pe>
2026-03-31 11:47:59 -0500

    fix: rename pg volume to v2 + polyfill performance API for jspdf

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,6 +2,16 @@ import { createInertiaApp } from '@inertiajs/react';
 import { createRoot } from 'react-dom/client';
 import '../css/app.css';
 
+// Polyfill para la Performance API requerida por jsPDF/html2canvas
+// en navegadores o entornos que no la implementan completamente
+if (typeof performance !== 'undefined') {
+    if (typeof performance.clearMarks !== 'function')   performance.clearMarks   = () => {};
+    if (typeof performance.mark !== 'function')         performance.mark         = () => {};
+    if (typeof performance.measure !== 'function')      performance.measure      = () => {};
+    if (typeof performance.getEntriesByName !== 'function') performance.getEntriesByName = () => [];
+    if (typeof performance.getEntriesByType !== 'function') performance.getEntriesByType = () => [];
+}
+
 createInertiaApp({
     title: (title) => title ? `${title} - Control de Deudas` : 'Control de Deudas',
     resolve: (name) => {
